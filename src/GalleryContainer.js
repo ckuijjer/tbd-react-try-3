dfc63442a9844dbba9f856c5eb290f31b4bf0e31
Casper Kuijjer <casper@kuijjer.com>
2017-06-07 07:04:20 +0200

    💄🐷

--- src/GalleryContainer.js
+++ src/GalleryContainer.js
@@ -16,23 +16,23 @@ class GalleryContainer extends Component {
   componentDidMount() {
     this.getImages();
   }
-  
+
   getImages() {
     return fetch(`https://www.reddit.com/r/${SUBREDDIT}/hot.json?raw_json=1&limit=24`)
-        .then(response => response.json())
-        .then(response => {
-            return response.data.children
-                .map(child => {
-                    const resolutions = _.get(child, 'data.preview.images[0].resolutions', [])
-                    return resolutions
-                        .filter(resolution => resolution.width === 320)
-                        .map(resolution => resolution.url)[0];
-                })
-                .filter(thumbnail => thumbnail !== undefined);
+      .then(response => response.json())
+      .then(response => {
+        return response.data.children
+          .map(child => {
+            const resolutions = _.get(child, 'data.preview.images[0].resolutions', [])
+            return resolutions
+              .filter(resolution => resolution.width === 320)
+              .map(resolution => resolution.url)[0];
           })
-          .then(images => {
-            this.setState({ images })
-          })
+          .filter(thumbnail => thumbnail !== undefined);
+      })
+      .then(images => {
+        this.setState({ images })
+      })
   }
 
   render() {
